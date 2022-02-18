2d9f314b619f4d8f82d8c2adf36d883fc712b777
Leonardo Freitas <leoafj@hotmail.com>
2022-02-18 11:01:29 -0300

    serializando YAML - YML

--- 03 RestWithSpringBoot/src/main/java/br/com/leo/config/WebConfig.java
+++ 03 RestWithSpringBoot/src/main/java/br/com/leo/config/WebConfig.java
@@ -1,14 +1,24 @@
 package br.com.leo.config;
 
+import br.com.leo.serialization.converter.YamlJackson2HttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+
+    private static final MediaType MEDIA_TYPE_YML = MediaType.valueOf("application/x-yaml");
+
+    public void extendMessageConverter(List<HttpMessageConverter<?>> converters){
+        converters.add(new YamlJackson2HttpMessageConverter());
+    }
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer){
@@ -38,6 +48,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
-                .mediaType("xml", MediaType.APPLICATION_XML);
+                .mediaType("xml", MediaType.APPLICATION_XML)
+                .mediaType("x-yaml", MEDIA_TYPE_YML);
     }
 }
