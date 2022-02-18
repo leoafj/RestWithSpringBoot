e68487201bdbf1bc7b28bcc95eb55c9788c39c5e
Leonardo Freitas <leoafj@hotmail.com>
2022-02-18 10:26:29 -0300

    content negotiation via QUERY parameter

--- 03 RestWithSpringBoot/src/main/java/br/com/leo/config/WebConfig.java
+++ 03 RestWithSpringBoot/src/main/java/br/com/leo/config/WebConfig.java
@@ -12,8 +12,18 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer){
-        configurer.favorParameter(false)
+
+        /*configurer.favorParameter(false)
                 .ignoreAcceptHeader(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);*/
+
+        configurer.favorPathExtension(false)
+                .favorParameter(true)
+                .parameterName("mediaType")
+                .ignoreAcceptHeader(true)
+                .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
                 .mediaType("xml", MediaType.APPLICATION_XML);
