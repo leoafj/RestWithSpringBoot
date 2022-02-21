3047c8321dfc609262f872d279ba86b2d579f9fb
Leonardo Freitas <leoafj@hotmail.com>
2022-02-21 16:43:36 -0300

    adicionando spring security, jwt e mapeando as entidades

--- 03 RestWithSpringBoot/src/main/java/br/com/leo/request/repository/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.leo.request.repository;
-
-import br.com.leo.data.model.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonRepository extends JpaRepository<Person, Long>{
-}
--- 03 RestWithSpringBoot/src/main/resources/db/migration/V2__Populate_Table_Person.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
-(2, 'JoÃ£o Pessoa - PB - Brasil', 'Leonardo', 'Male', 'Freitas'),
-(3, 'JoÃ£o Pessoa - PB - Brasil', 'Gabriela', 'Female', 'Freitas'),
-(4, 'JoÃ£o Pessoa - PB - Brasil', 'Rafael', 'Male', 'Freitas');
