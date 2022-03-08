de3a6d356dccac32f634f5bd0881e886552acd24
Leonardo Freitas <leoafj@hotmail.com>
2022-03-08 16:37:34 -0300

    implementando o download de arquivos

--- RestWithSpringBoot/src/main/java/br/com/erudio/services/FileStorageService.java
+++ RestWithSpringBoot/src/main/java/br/com/erudio/services/FileStorageService.java
@@ -6,13 +6,17 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import br.com.erudio.config.FileStorageConfig;
 import br.com.erudio.exception.FileStorageException;
+import br.com.erudio.exception.MyFileNotFoundException;
 
 @Service
 public class FileStorageService {
@@ -47,6 +51,21 @@ public class FileStorageService {
 			throw new FileStorageException("Could not store file" + fileName + ". Please try again!", e);
 		}
 	}
+	
+	public Resource loadFileAsResource(String fileName) {
+		try {
+			Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
+			Resource resource = new UrlResource(filePath.toUri());
+			if (resource.exists()) {
+				return resource;
+			} else {
+				throw new MyFileNotFoundException("File not found" + fileName);
+			}
+		} catch (Exception e) {
+			throw new MyFileNotFoundException("File not found" + fileName, e);
+		}
+	}
+	
 }
 
 
